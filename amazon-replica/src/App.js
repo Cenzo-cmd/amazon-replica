b143492d36f565849b40ebb22730cb673caf30fd
Vincent <vrdphone@gmail.com>
2020-12-11 15:31:30 -0600

    added Nav Bar

---
+++ amazon-replica/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from "./components/Header/Header";
+import Body from "./components/Body/Body";
+
+function App() {
+  return (
+    <div className="app">
+      
+      <Header />
+      
+      <Body />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ amazon-replica/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDlekERaxTFjRIYVlbn_GsDU35Z-DA8_h0",
+    authDomain: "replica-cfba2.firebaseapp.com",
+    projectId: "replica-cfba2",
+    storageBucket: "replica-cfba2.appspot.com",
+    messagingSenderId: "407232643523",
+    appId: "1:407232643523:web:9e03675a3a6eea8a34bd76"
+};
